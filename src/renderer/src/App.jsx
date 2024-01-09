372b49c09036694030035b85985a169c42060073
Wiiiiill <648130397@qq.com>
2024-01-09 20:53:04 +0800

    update

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,8 +1,23 @@
+import { read, utils } from 'xlsx'
+import fs from 'file-saver'
+import XLSX from 'xlsx' // 引入xlsx
 import dayjs from 'dayjs'
 import { useState, useEffect } from 'react'
 import { FaTimes } from 'react-icons/fa'
 import Footer from './Footer'
 !localStorage.getItem('dates') && localStorage.setItem('dates', JSON.stringify([]))
+const s2ab = (s) => {
+  if (typeof ArrayBuffer !== 'undefined') {
+    let buf = new ArrayBuffer(s.length)
+    let view = new Uint8Array(buf)
+    for (let i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xff
+    return buf
+  } else {
+    let buf = new Array(s.length)
+    for (let i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xff
+    return buf
+  }
+}
 export default function App() {
   const [ram, setRam] = useState(1)
   ram
@@ -139,12 +154,70 @@ export default function App() {
     dates.splice(i, 1, { ...e, reminder: !e.reminder })
     setDates([...dates])
   }
+  const donwoad = () => {
+    let datas = dates.map((e) => [e.code, e.time, e.t3, e.t5])
+    let header = ['编号', '时间', '3.3天后', '5天后']
+    let sheetData = [header, ...datas]
+    let sheet = XLSX.utils.aoa_to_sheet(sheetData)
+    let maxWidth = header.map((e) => {
+      return { wch: e.length * 3 }
+    })
+    for (let index = 0; index < datas.length; index++) {
+      for (let i = 0; i < datas[index].length; i++) {
+        if ((datas[index][i] + '').toString().length * 1.2 > maxWidth[i].wch) {
+          maxWidth[i].wch = (datas[index][i] + '').toString().length * 1.2
+        }
+      }
+    }
+    sheet['!cols'] = maxWidth
+    let wb = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(wb, sheet, 'Sheet1')
+    const defaultCellStyle = {
+      font: { name: 'Verdana', sz: 13, color: 'FF00FF88' },
+      fill: { fgColor: { rgb: 'FFFFAA00' } }
+    } //设置表格的样式
+    let wopts = {
+      bookType: 'xlsx',
+      bookSST: false,
+      type: 'binary',
+      cellStyles: true,
+      defaultCellStyle: defaultCellStyle,
+      showGridLines: false
+    } //写入的样式
+    let wbout = XLSX.write(wb, wopts)
+    let blob = new Blob([s2ab(wbout)], {
+      type: 'application/octet-stream'
+    })
+    fs.saveAs(blob, `${dayjs().format('YYYY-MM-DD HH:mm:ss')}.xlsx`)
+  }
+  const chooseFile = async (e) => {
+    if (e.target.files.length) {
+      let file = e.target.files[0]
+      let arrayBuffer = await file.arrayBuffer()
+      let data = read(arrayBuffer)
+
+      if (data.Sheets && data.SheetNames.length) {
+        let datas = utils.sheet_to_json(data.Sheets[data.SheetNames[0]])
+        let t = datas.map((obj) => {
+          return {
+            code: obj['编号'],
+            time: dayjs(new Date(obj['时间'])).format('YYYY-MM-DD HH:mm:ss'),
+            t3: getT3(obj['时间']),
+            t5: getT5(obj['时间']),
+            reminder: true
+          }
+        })
+        setDates([...t, ...dates])
+      }
+    }
+  }
+  const upload = () => {}
   return (
     <div>
       <div className="container">
         <header className="header">
           <h2>当前时间:{dayjs().format('YYYY-MM-DD HH:mm:ss')}</h2>
-          {!showAdd && (
+          {/* {!showAdd && (
             <button
               className="btn"
               onClick={() => {
@@ -165,17 +238,32 @@ export default function App() {
             >
               取消
             </button>
-          )}
-          <div>
+          )} */}
+
+          <div className="full" align="right">
             <button
               className="btn"
               onClick={() => {
-                setShowAdd(false)
+                donwoad()
               }}
-              style={{ backgroundColor: 'red' }}
             >
-              取消
+              导出
             </button>
+            <button
+              className="btn"
+              onClick={() => {
+                document.getElementById('uploadBtn').click()
+              }}
+            >
+              导入
+            </button>
+            <input
+              onChange={chooseFile}
+              id="uploadBtn"
+              type="file"
+              style={{ display: 'none' }}
+              accept=".xls,.xlsx"
+            />
           </div>
         </header>
         {showAdd && (
